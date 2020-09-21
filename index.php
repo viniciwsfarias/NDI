e17309f2269ee413bd07cea0ac3cdbac0a885d61
Viniciwsfarias <55116290+viniciwsfarias@users.noreply.github.com>
2020-09-21 14:50:18 -0300

    Add files via upload

---
+++ index.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+
+	<title>NDI</title>
+	  <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+  
+	
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+
+	<style type="text/css">
+		.text_title{
+			font-size: 30px;
+			color: #fff;
+		}
+		.space{
+			margin: 50px;
+			padding: 50px;
+		}
+		.display-4{
+			font-size: 35px;
+			font-family: 'Roboto';
+			text-align: center;
+		}
+
+	</style>
+
+	
+</head>
+
+
+<body>
+	<!-- menu -->
+	<nav class="navbar navbar-expand-md navbar-light sticky-top">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#"><img src="imagens/ColégioNDI.png" width="196" height="132">
+				<button class="navbar-toggler navbar-toggler-left" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarResponsive">
+					<ul class="navbar-nav ml-auto">
+						<li class="nav-item">
+							<a class="nav-item" href="#"></a>
+						</li>
+						<li class="nav-item active">
+							<a class="nav-link" href="#">Home</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">O Colégio</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Ensinos</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Espaço On-line</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Instagram</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Fale Conosco</a>
+						</li>
+						
+					</ul>
+
+		</div>
+
+	</nav>
+	
+	<!-- slide show -->
+
+	<div id="slides" class="carousel slide" data-ride="carousel">
+		<ul class="carousel-indicators">
+			<li data-target="#slides" data-slide-to="0" class="active"></li>
+			<li data-target="#slides" data-slide-to="1"></li>
+			<li data-target="#slides" data-slide-to="2"></li>
+		</ul>
+		<div class="carousel-inner">
+			<div class="carousel-item active">
+				<img class="d-block w-100" src="imagens/slide1.jpg">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="imagens/slide2.jpg">
+			</div>
+			<div class="carousel-item">
+				<img class="d-block w-100" src="imagens/slide3.jpg">
+			</div> 
+		</div>
+		<a class="carousel-control-prev" href="#slides" role="button" data-slide="prev">
+			<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+			<span class="sr-only">Anterior</span>
+		</a>
+			<a class="carousel-control-next" href="#slides" role="button" data-slide="next">
+			<span class="carousel-control-next-icon" aria-hidden="true"></span>
+			<span class="sr-only">Próxima</span>
+		</a>
+	</div>
+
+	
+	
+
+	<!-- O Colégio -->
+	<div class="space">
+		<div class="container-fluid padding">
+			<div class="row padding">
+				<div class="col-lg-6">
+					<img src="imagens/slide3.jpg"  class="img-fluid">
+				</div>
+				<div class="col-lg-6" style="background: #f8c346;">
+					<div class="text_title"> Um pouco da nossa história</div>
+				<br>
+				O Colégio NDI – Núcleo de Desenvolvimento Intelectual é uma instituição de ensino com turmas do turmas do Infantil ao terceiro ano do Ensino Médio, e tem como foco o pleno desenvolvimento da criança e do jovem em suas habilidades e competências, prezando pelo ensino de excelência como também pela formação humanística dos seus alunos.<br>
+
+				O início de sua trajetória como instituição remonta ao começo dos anos 2000 quando começou a atuar como cursinho pré-vestibular. Em julho de 2001, identificou-se no mercado uma oportunidade de expansão, assim, foi elaborado um planejamento a fim de implantar o novo empreendimento. Foi então que em 2002, o NDI tornou-se Colégio atuando com turmas do Ensino Fundamental II e Ensino Médio.<br>
+
+				Ampliando a proposta pedagógica, em 2015 foi introduzido o segmento da Educação Infantil, finalizando deste modo, sua formação atual. De cursinho a colégio, o NDI gradativamente foi transformando-se na instituição que é hoje. E nestes anos de existência, vem oferecendo à comunidade campinense, dedicação e comprometimento nos serviços educacionais prestados.
+				</div>
+			</div>
+		</div>
+	</div>
+
+	<!-- Aréa de atuação  -->
+		<div class="jumbotron" style="background: #f8c346">
+			<h1 class="display-4">ÁREAS DE ATUAÇÃO</h1>
+		</div>
+		
+
+
+
+	<!--  -->
+
+	<div class="container-fluid padding">
+		
+			<div class="row text-center padding">
+				<div class="col-xs-12 col-sm-6 col-md-4">
+					<img src="imagens/img2.png" width="191" height="164">
+					<button type="button" class="btn btn-primary">EDUCAÇÃO INFANTIL</button>
+				</div>
+				<div class="col-xs-12 col-sm-6 col-md-4">
+					<img src="imagens/img3.png" width="95" height="168">
+					<button type="button" class="btn btn-primary">ENSINO FUNDAMENTAL</button>
+				</div>
+				<div class="col-sm-12 c col-md-4">
+					<img src="imagens/img4.png" width="218" height="189">
+					<button type="button" class="btn btn-primary">ENSINO MÉDIO</button>
+				</div>
+			</div>
+		
+	</div>
+
+
+	<div class="jumbotron" style="background: #29679b">
+			<h1 class="display-4" style="color: #f8c346">O QUE ACONTECE NO NOSSO INSTAGRAM</h1>
+	</div>
+	<div class="container">
+			  <!-- LightWidget WIDGET --><script src="https://cdn.lightwidget.com/widgets/lightwidget.js"></script><iframe src="//lightwidget.com/widgets/83912506537753f18fbd60c3c6ea6435.html" scrolling="no" allowtransparency="true" class="lightwidget-widget" style="width:100%;border:0;overflow:hidden;"></iframe>
+
+	</div>
+
+	<div class="jumbotron" style="background: #f8c346">
+		<h1 class="display-4"> NOSSO CANAL</h1>
+	</div>
+	<dir class="container">
+		<iframe id="ytplayer" type="text/html" width="100%" height="405"
+		src="https://www.youtube.com/embed/?listType=playlist&list=PL590L5WQmH8eYTXiTTjzo4YnK7vkCqZTI"
+		frameborder="0" allowfullscreen></iframe>
+
+	</div>
+		
+	</dir>
+	<div class="jumbotron" style="background: #f8c346">
+		<h1 class="display-4"> FALE CONOSCO</h1>
+	</div>
+	<form>
+		<div class="form-row">
+			<div class="form-group col-md-6">
+				<label for="Name">Seu Nome:</label>
+				<input type="text" class="form-control" placeholder="Seu nome" name="nome" id="nome">
+			</div>
+			<div class="form-group col-md-6">
+				<label for="Email">E-mail:</label>
+				<input type="email" class="form-control" placeholder="Seu e-mail" name="email" id="email">
+			</div>
+			<div class="form-group col-md-6">
+				<label for="Tel">Telefone:</label>
+				<input type="text" class="form-control" placeholder="Seu Telefone" name="tel" id="tel">
+			</div>
+			<div class="form-group col-md-6">
+				<label for="Assunto">Assunto:</label>
+				<input type="text" class="form-control" placeholder="Assunto" name="assunto" id="assunto">
+			</div>
+			<div class="form-group">
+				<label for="Message">Sua Mensagem</label>
+				<textarea class="form-control" id="messagem" ></textarea>
+			</div>
+		</div>
+			<button type="submit" name="submit" class="btn btn-warning btn-lg btn-block">Enviar mensagem</button>
+	</form>
+	<div class="container">
+		<iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3958.2474602513007!2d-35.88656578531054!3d-7.212587072816188!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x7ac1fb2f68d5e63%3A0xc421ef30c3c5a009!2sR.%20Vigolvino%20Vanderlei%2C%20535%20-%20Concei%C3%A7%C3%A3o%2C%20Campina%20Grande%20-%20PB%2C%2058401-311!5e0!3m2!1spt-BR!2sbr!4v1598296693647!5m2!1spt-BR!2sbr" width="100%" height="450" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+	</div>
+
+	</nav>
+	<footer>
+		<div class="container-fluid padding" style="background: black">
+			<div class="row text-center" style="color:#f8c346 ">
+				<div class="col">
+					<img src="imagens/logo_sed.png">
+					<hr class="light">
+					<p>2020 - Desenvolvido por SED Soft</p>
+				</div>
+			</div>
+		</div>
+	</footer>
+
+	
+</body>
+</html>
